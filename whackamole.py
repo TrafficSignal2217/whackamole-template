e9b30da5401fd57966ec0b0c629f3863263cfa1c
TrafficSignal2217 <loganbrown2217@gmail.com>
2024-11-16 13:38:43 -0500

    version 1.0

--- whackamole.py
+++ whackamole.py
@@ -1,12 +1,17 @@
 import pygame
+import random
 
+# :exe 'edit '.stdpath('config').'/init.vim'
+
+
+#def mouse_in_rect(top_left, bottom_right):
+    #if pygame.mouse.get_pos()[0] > 
 
 def main():
     tile_size = 32
     screen_width = 640
     screen_height = 512
-
-
+    color_bg = "light green"
 
     try:
         pygame.init()
@@ -20,10 +25,22 @@ def main():
 
         # Game loop
         while running:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        main()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if mole_rect.collidepoint(event.pos):
+                        mole_x = random.randint(1, screen_width // tile_size - 1) * tile_size
+                        mole_y = random.randint(1, screen_height // tile_size - 1) * tile_size
+                        mole_rect.topleft = (mole_x, mole_y)
+            screen.fill(color_bg)
+
+            #if pygame.mouse.get_pressed(3)[0]:
+
 
             # Render game
             for y in range(screen_height // tile_size):
@@ -32,7 +49,7 @@ def main():
             for x in range(screen_width // tile_size):
                 pygame.draw.line(screen, "blue", (x * tile_size, 0), (x * tile_size, screen_height))
 
-            screen.blit(mole_image, mole_rect)
+            screen.blit(mole_image, (mole_x, mole_y))
 
             # Flip display to draw to screen
             pygame.display.flip()
